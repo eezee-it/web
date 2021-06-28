e20e3348e7be8eebea3d38f8443fd2fb134dbb2c
mohsen <mohsen.dhifallah@eezee-it.com>
2021-06-28 17:17:03 +0100

    Add OCA website sort product

--- website_sale_product_sort/models/website.py
+++ website_sale_product_sort/models/website.py
@@ -1,23 +1,5 @@
-# -*- coding: utf-8 -*-
-# ############################################################################
-#
-#    Copyright Eezee-It (C) 2021
-#    Author: Eezee-It <info@eezee-it.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
 
 
